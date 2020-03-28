13eab3332b9bff304d5d8ca8146a9ab76685a234
Nikhil <nikhil97.nb@gmail.com>
2020-03-28 15:27:38 +0530

    🎉 Implemented overshooting margin animation! 🎉

--- app/src/main/java/app/nikhil/instagramgalleryanimation/ui/MainActivity.kt
+++ app/src/main/java/app/nikhil/instagramgalleryanimation/ui/MainActivity.kt
@@ -1,9 +1,13 @@
 package app.nikhil.instagramgalleryanimation.ui
 
 import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.OvershootInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import app.nikhil.instagramgalleryanimation.R
 import app.nikhil.instagramgalleryanimation.utils.TEST_IMAGES
 import com.bumptech.glide.Glide
@@ -24,11 +28,34 @@ class MainActivity : AppCompatActivity(), InstagramView.InstagramViewListener {
     blurView.visibility = View.VISIBLE
     imageView_expanded.visibility = View.VISIBLE
 
+    // Load the image in the image view.
     Glide.with(this@MainActivity)
       .load(imageUrl)
       .into(imageView_expanded)
 
-    val blurAnimator = ObjectAnimator.ofFloat(blurView, "alpha", 0F, 0.6F)
+    startBlurAnimation()
+    startMarginAnimation()
+  }
+
+  private fun startMarginAnimation() {
+    val marginAnimator = ValueAnimator.ofInt(80, 20)
+      .apply {
+        interpolator = OvershootInterpolator(2F)
+        duration = 300
+        addUpdateListener {
+          val value = it.animatedValue as Int
+          val params = (imageView_expanded.layoutParams as ConstraintLayout.LayoutParams).apply {
+            leftMargin = value
+            rightMargin = value
+          }
+          imageView_expanded.layoutParams = params
+        }
+      }
+    marginAnimator.start()
+  }
+
+  private fun startBlurAnimation() {
+    val blurAnimator = ObjectAnimator.ofFloat(blurView, View.ALPHA.name, 0F, 0.6F)
     blurAnimator.start()
   }
 
@@ -37,7 +64,32 @@ class MainActivity : AppCompatActivity(), InstagramView.InstagramViewListener {
   }
 
   override fun onAnimationStop() {
+    reverseBlurAnimation()
+    reverseMarginAnimation()
+
     blurView.visibility = View.GONE
     imageView_expanded.visibility = View.GONE
+  }
+
+  private fun reverseMarginAnimation() {
+    val marginAnimator = ValueAnimator.ofInt(20, 80)
+      .apply {
+        interpolator = AccelerateInterpolator()
+        duration = 300
+        addUpdateListener {
+          val value = it.animatedValue as Int
+          val params = (imageView_expanded.layoutParams as ConstraintLayout.LayoutParams).apply {
+            leftMargin = value
+            rightMargin = value
+          }
+          imageView_expanded.layoutParams = params
+        }
+      }
+    marginAnimator.start()
+  }
+
+  private fun reverseBlurAnimation() {
+    val blurAnimator = ObjectAnimator.ofFloat(blurView, View.ALPHA.name, 0.6F, 0F)
+    blurAnimator.start()
   }
 }
